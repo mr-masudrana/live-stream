81b7576bd1aae563ac7f765807f4230ced3f1031
Masud Rana <71147447+mr-masudrana@users.noreply.github.com>
2025-07-12 05:51:47 +0600

    Update data.js

--- api/data.js
+++ api/data.js
@@ -3,21 +3,35 @@ export const channels = [
     id: "asiantv",
     name: "Asian TV",
     group: "Bangla",
-    url: "asian-test-sample-ok-d.stream",
+    raw_url: "asian-test-sample-ok-d.stream",
     logo: "https://www.lyngsat.com/logo/tv/aa/asian-tv-bd.png"
+  },
+  {
+    id: "atnbangla",
+    name: "ATN Bangle",
+    group: "Bangla",
+    raw_url: "atn-bangle.stream",
+    logo: "https://www.lyngsat.com/logo/tv/aa/atn-bangla-bd.png"
+  },
+  {
+    id: "channeli",
+    name: "Channel I",
+    group: "News",
+    raw_url: "channel-i-.stream",
+    logo: "https://www.lyngsat.com/logo/tv/cc/channeli_bd.png"
   },
   {
     id: "channel24",
     name: "Channel 24",
     group: "News",
-    url: "channel24-sg-e8e.stream",
+    raw_url: "channel24-sg-e8e.stream",
     logo: "https://www.lyngsat.com/logo/tv/cc/channel24_bd.png"
   },
   {
     id: "somoytv",
     name: "Somoy TV",
     group: "News",
-    url: "somoyt000011226615544544.stream",
+    raw_url: "somoyt000011226615544544.stream",
     logo: "https://www.lyngsat.com/logo/tv/ss/somoy_news.png"
   }
 ];
